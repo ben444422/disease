7a4932c8cbdbe904abe3e782a6ab50c54f482168
ben <ben444422@gmail.com>
2015-06-27 21:50:42 -0700

    .

--- testsql.py
+++ testsql.py
@@ -1,4 +1,3 @@
-
 import MySQLdb
 import sys
 import csv
@@ -6,39 +5,10 @@ import csv
 
 
 # Open database connection
-db = MySQLdb.connect("127.0.0.1","root","root","disease" )
+db = MySQLdb.connect("127.0.0.1","root","root","gidx")
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
-
-
-#first fill the symptoms
-
-
-
-diseases = {}
-
-
-symptoms = {}
-
-
-
-with open('diseasexsymptoms.csv', 'rU') as f:
-    reader = csv.reader(f)
-    for row in reader:
-    	print row
-
-
-
-
-
-
-
-
-
-
-
-
 
 # execute SQL query using execute() method.
 cursor.execute("SELECT VERSION()")
